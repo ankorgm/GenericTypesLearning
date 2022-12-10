1dde1e085d77f58c13a31ebafca5f3a31f9bb1cb
ankorgm <ankorgm@gmail.com>
2022-12-11 01:55:32 +0300

    Generic types with deep 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -38,31 +39,61 @@ public class Main {
 
         ArrayList<Integer> numbers = new ArrayList(); //сохраняет Object
 
-        for (int i = 0; i<10; i++){ // сохраняет в коллекцию числа 10, 20 ... 100
-            numbers.add(i*10);
-           // numbers.add(i*10.4f);
+        for (int i = 0; i < 10; i++) { // сохраняет в коллекцию числа 10, 20 ... 100
+            numbers.add(i * 10);
+            // numbers.add(i*10.4f);
 
         }
 
         //numbers.add(10.4f);
 
         int sum = 0;
-        for (Integer o : numbers){
+        for (Integer o : numbers) {
             sum = sum + /*(Integer)*/ o;
 
         }
         System.out.println(sum);
 
+        // ОсновнойТип <ТипПараметр>
+        // ОсновнойТип <ТипПараметр1, ТипПараметр2,ТипПараметр3,ТипПараметр4>
+
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(7, "Privet");
+        map.put(15, "Hello");
+
+        ArrayList<String> listHello = new ArrayList<>();
+        listHello.add("Privet");
+        listHello.add("Hi");
 
 
+        ArrayList<String> listBye = new ArrayList<>();
+        listBye.add("Poka");
+        listBye.add("Good Bye");
 
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        list.add(listHello);
+        list.add(listBye);
 
-
-
-
+        for (ArrayList<String> spisok : list){
+            for (String s : spisok){
+                System.out.println(s);
+            }
+        }
 
 
     }
 
 
 }
+
+
+/*
+ *     то что делаем мы в коде                                     то во что преобразует компилятор
+ *   ArrayList<Integer> list = new ArreyList<Integer>();        ArrayList list = new ArrayList
+ *   list.add(1);                                               list.add((Integer) 1);
+ *   int x = list.get(0);                                       int x = (Integer) list.get(0);
+ *    list.set(0, 10);                                          list.set(0, (Integer) 10);
+ *
+ */
+
+
