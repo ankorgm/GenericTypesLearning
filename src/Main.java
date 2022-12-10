3ade8815d5c436066fff90d562eae3a4e0a7be53
ankorgm <ankorgm@gmail.com>
2022-12-11 01:03:08 +0300

    Finish with example of cast failure in ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
 
 
     public static void main(String[] args) {
@@ -25,16 +25,31 @@ public class Main {
 
 */
 
-     Object[] objects = {10, "Привет", 3.14}; // Здесь происходит автоупаковка
+        Object[] objects = {10, "Привет", 3.14}; // Здесь происходит автоупаковка
 
-     for (Object o: objects){
-         if (o instanceof String){
-             String s = (String) o;
-             System.out.println(s);
-         }
+        for (Object o : objects) {
+            if (o instanceof String) {
+                String s = (String) o;
+                System.out.println(s);
+            }
 
 
-     }
+        }
+
+        ArrayList numbers = new ArrayList(); //сохраняет Object
+
+        for (int i = 0; i<10; i++){ // сохраняет в коллекцию числа 10, 20 ... 100
+            numbers.add(i*10);
+           // numbers.add(i*10.4f);
+
+        }
+
+        int sum = 0;
+        for (Object o : numbers){
+            sum = sum + (Integer) o;
+
+        }
+        System.out.println(sum);
 
 
     }
