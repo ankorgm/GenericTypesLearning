7eab4d832f6d5af5009c20068727fd242a8d1919
ankorgm <ankorgm@gmail.com>
2022-12-11 00:25:51 +0300

    First example of type casting

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
 
 
     public static void main(String[] args) {
+         Object o = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+
+
+
+        // System.out.println(o instanceof Scanner);
+
+        Scanner scanner = null;
+        if (o instanceof Scanner) scanner  = (Scanner) o;
+
+         if (scanner != null){
+             scanner.nextInt();
+         }
+
+         Object o1 = new String();
+
+
     }
-
-
-
-
 
 
 }
