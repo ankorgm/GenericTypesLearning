a89aead71408fc796a5a95704f3b2aed8ec4e573
ankorgm <ankorgm@gmail.com>
2022-12-12 00:33:21 +0300

    Constructor only generic implementation

--- src/ru/synergy/customgenerictypes/Main.java
+++ src/ru/synergy/customgenerictypes/Main.java
@@ -13,5 +13,11 @@ public class Main {
         int a = acc1.getId() + 10; // Опасное преобразование типов
         System.out.println(a);
 
+
+
+
+        Account<Integer> acc3 = new Account(1300, 100, "Something");
+
+
     }
 }
